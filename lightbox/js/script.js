b60e1b93e2a4dddf52d708c92ede8264dd07bda5
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-08-04 01:24:43 +0900

    Add blank

--- lightbox/js/script.js
+++ lightbox/js/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
+  // 변수 초기화
   var $thumb = $(".thumb");
   var $backdrop = $("#backdrop");
   var $closeBtn = $(".btn.close");
   var $box = $("#lightbox .box");
 
-  //라이트 박스 켜기
+  // 라이트 박스 켜기
   function openLightbox(index) {
     $("#image-" + index)
       .fadeIn("slow")
       .addClass("active");
     $backdrop.fadeIn("slow").addClass("active");
   }
-  //라이트 박스 닫기
+  // 라이트 박스 닫기
   function closeLightbox() {
     $box.fadeOut("slow").removeClass("active");
     $backdrop.fadeOut("slow").removeClass("active");
   }
 
-  //썸네일 클릭 이벤트
+  // 썸네일 클릭 이벤트
   $thumb.click(function() {
     var index = $(this).index() + 1;
     openLightbox(index);
   });
 
-  //라이트 박스 닫기 버튼 이벤트
+  // 라이트 박스 닫기 버튼 이벤트
   $closeBtn.click(function() {
     closeLightbox();
   });
-  //이미지 전환 이벤트
 });
